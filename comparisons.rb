a4643701dd18af8e1beefcd73544e6202401633a
Tyler Noble <tmnoble11@gmail.com>
2024-04-28 16:50:36 -0600

    completed all of the prompts on comparison.rb.

--- comparisons.rb
+++ comparisons.rb
@@ -54,20 +54,24 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
+## this example uses the less than comparison operator to check if 4 is less than 9. Puts returns boolean value of true.
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+## this example is using the the less than comparison operator to check if 4 is less that the stored value of books being 3. Puts returns boolean value of false.
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+##This example is using the greater than comparison operator to check if the value of friends is greater than siblings. Puts returns the boolean value of true.
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+##This example is using the is not comparison operator to check if the value of attendees is not the value of meals. Puts returns the boolean value of true.
 
 
 #-------------------
@@ -90,16 +94,34 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
+# this is the and logical operator, it is comparing if both loves to play and loves treats are true. They both are so puts will return the boolean true.
 
 # Determine if the dog loves to play and loves the dog park
-
-
-# Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play && loves_dog_park
+#this is the and logical operator, it is comparing if loves to play and loves dog park are true. Since one value is true and the other is false, puts will return the boolean value of false.
 
 # Determine if the dog loves to play and is a puppy
+
+#made a method to determine if the dog is a puppy or an adult by passing in the argument of the stored variable ^age. once conditions
+#are checked with the if else statement, pup_or_adult is returned globally and stored under pup_or_adult.
+def age_determin(dog_age)  
+    
+    if dog_age < 2
+        pup_or_adult = 'puppy'
+    else 
+        pup_or_adult = 'adult'
+    end
+    return pup_or_adult
+end
+pup_or_adult = age_determin(age)
+
+puts loves_to_play && pup_or_adult == 'puppy'
+ 
+
+
 # Assume that a dog is a puppy if it is less than 2 years old.
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: My final line of code evaluated to true. The reasoning is because comparing both loves_to_play and pup_or_adult with the 
+# logical operator && will check to see if both sides of the operator equate to True. In this case puts will print the boolean value of true.
