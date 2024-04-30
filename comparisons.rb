0f8f35d6a525db5dba44cd187bac2e66df23c43a
Tyler Noble <tmnoble11@gmail.com>
2024-04-30 11:39:46 -0600

    Finished part 1 in comparisons.rb

--- comparisons.rb
+++ comparisons.rb
@@ -20,28 +20,28 @@ numberDogs = 0
 # EXAMPLE: print the result of the comparison: is number_seachers greater than number_students?
 puts "Is number_teachers greater than number_students?", number_teachers > number_students
 # this should print: "Is numberTeachers greater than numberStudents?" false
-
+puts "Is numberTeachers greater than numberStudents?", number_teachers > number_students
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
-
+puts number_teachers < number_students
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
-
+puts number_teachers == string_teachers
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
-
+puts number_teachers != number_students
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
-
+puts number_students >= 20
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
-
+puts number_students >= 21
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
-
+puts number_students <= 20
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
-
+puts number_students <= 21
 
 #-------------------
 # PART 2: Articulating what you are doing
@@ -54,7 +54,7 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
-## this example uses the less than comparison operator to check if 4 is less than 9. Puts returns boolean value of true.
+## this example uses the less than comparison operator to check if 4 is less than 9. Puts prints the boolean true.
 
 books = 3
 puts 4 < books
